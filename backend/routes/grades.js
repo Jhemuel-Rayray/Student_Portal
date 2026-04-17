bf5363090d89d80e675e23703e41a9a6c4449f71
Jhemuel Rayray <jhemuel.rayray1@gmail.com>
2026-04-17 18:20:44 +0800

    Fix duplicate declarations in server.js

--- backend/routes/grades.js
+++ backend/routes/grades.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { auth, adminOnly, facultyOnly } = require('../middleware/auth');
-const { getGrades, createGrade, updateGrade, deleteGrade } = require('../controllers/gradeController');
 
+// TAMA: Import as a single function dahil 'module.exports = verifyToken' ang nasa auth.js
+const auth = require('../middleware/auth');
+
+// TAMA: Siguraduhin na 'gradesController' (may S) ang filename
+const { getGrades, createGrade, updateGrade, deleteGrade } = require('../controllers/gradesController');
+
+// Routes
 router.get('/:student_id', auth, getGrades);
-router.post('/', auth, facultyOnly, createGrade);
-router.put('/:id', auth, facultyOnly, updateGrade);
-router.delete('/:id', auth, facultyOnly, deleteGrade);
+router.post('/', auth, createGrade);
+router.put('/:id', auth, updateGrade);
+router.delete('/:id', auth, deleteGrade);
 
 module.exports = router;
